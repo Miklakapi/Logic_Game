0f495302461bb7abe5cb1cd30ba3eaa1fe4698b8
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-11 00:45:56 +0100

    Create Data.cpp

---
+++ Logic_Game/Logic_Game/Data.cpp
@@ -0,0 +1,17 @@
+#include "Data.hpp"
+
+int Data::asHealth() {
+	return health;
+}
+
+int Data::asLV() {
+	return lv;
+}
+
+int Data::asMinutes() {
+	return minutes;
+}
+
+int Data::asSeconds() {
+	return seconds;
+}
