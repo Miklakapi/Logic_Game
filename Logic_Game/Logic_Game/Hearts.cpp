110e560b0b11320563a84f6821fbe72bb2f1dd94
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-10 20:37:46 +0100

    Create Hearts.cpp

---
+++ Logic_Game/Logic_Game/Hearts.cpp
@@ -0,0 +1,70 @@
+#include "Hearts.hpp"
+
+Texture* Hearts::texture;
+
+Hearts::Hearts(Vector2f position) {
+	heart = new RectangleShape[3];
+	for (int i = 0; i < 3; i++) {
+		(heart + i)->setSize(Vector2f{ 50,50 });
+		(heart + i)->setTexture(texture);
+	}
+	setPosition(position);
+	setHealth(3);
+}
+
+void Hearts::setTexture(string textureFile) {
+	static int n = 0;
+	if (n == 0) {
+		texture = new Texture;
+		n++;
+	}
+	texture->loadFromFile(textureFile);
+}
+
+void Hearts::setPosition(Vector2f position) {
+	(heart + 0)->setPosition(position);
+	(heart + 1)->setPosition(position + Vector2f{ 55,0 });
+	(heart + 2)->setPosition(position + Vector2f{ 110,0 });
+}
+
+void Hearts::setHealth(int health) {
+	this->health = health;
+	switch (health) {
+	case 0:
+		(heart + 0)->setTextureRect(IntRect{ 0,0,1,1 });
+		(heart + 1)->setTextureRect(IntRect{ 0,0,1,1 });
+		(heart + 2)->setTextureRect(IntRect{ 0,0,1,1 });
+		break;
+	case 1:
+		(heart + 0)->setTextureRect(IntRect{ 0,0,80,80 });
+		(heart + 1)->setTextureRect(IntRect{ 0,0,1,1 });
+		(heart + 2)->setTextureRect(IntRect{ 0,0,1,1 });
+		break;
+	case 2:
+		(heart + 0)->setTextureRect(IntRect{ 0,0,80,80 });
+		(heart + 1)->setTextureRect(IntRect{ 0,0,80,80 });
+		(heart + 2)->setTextureRect(IntRect{ 0,0,1,1 });
+		break;
+	case 3:
+		(heart + 0)->setTextureRect(IntRect{ 0,0,80,80 });
+		(heart + 1)->setTextureRect(IntRect{ 0,0,80,80 });
+		(heart + 2)->setTextureRect(IntRect{ 0,0,80,80 });
+		break;
+	default:
+		this->health = 3;
+		(heart + 0)->setTextureRect(IntRect{ 0,0,80,80 });
+		(heart + 1)->setTextureRect(IntRect{ 0,0,80,80 });
+		(heart + 2)->setTextureRect(IntRect{ 0,0,80,80 });
+		break;
+	}
+}
+
+int Hearts::getHealth() {
+	return health;
+}
+
+void Hearts::draw(RenderWindow& window) {
+	for (int i = 0; i < 3; i++) {
+		window.draw(*(heart + i));
+	}
+}
