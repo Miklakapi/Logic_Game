003b674888b448dcb03d39835d3cdc9d2076ea3a
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-10 20:37:34 +0100

    Create MapReader.hpp

---
+++ Logic_Game/Logic_Game/MapReader.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include <string>
+#include <fstream>
+#include "Square.hpp"
+
+using namespace std;
+using namespace sf;
+
+class MapReader{
+
+	Type* type;
+
+	int actualLv;
+
+public:
+
+	MapReader();
+
+	void loadLV(int lv);
+
+	void renderLV(Square* square);
+
+	void renderLV(int lv, Square* square);
+
+	int getActualLV();
+
+	~MapReader();
+};
+
