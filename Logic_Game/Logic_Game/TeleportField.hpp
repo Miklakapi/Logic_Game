6ee1ab37715f5ed568c755e980ce6744298e2c84
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-10 20:37:40 +0100

    Create TeleportField.hpp

---
+++ Logic_Game/Logic_Game/TeleportField.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+
+using namespace sf;
+
+class TeleportField{
+
+public:
+
+	TeleportField();
+	~TeleportField();
+
+};
+
