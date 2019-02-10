c3be3eeb6e4419634e04b5e57b993196fd3c55fe
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-11 00:46:19 +0100

    Update Timer.hpp

--- Logic_Game/Logic_Game/Timer.hpp
+++ Logic_Game/Logic_Game/Timer.hpp
@@ -2,14 +2,35 @@
 #include <SFML\Window.hpp>
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
+#include <string>
 
 using namespace sf;
+using namespace std;
 
 class Timer : public Clock{
 
+	Font font;
+
+	Text text;
+
+	Clock clock;
+
+	int minutes;
+
+	int seconds;
 
 public:
 
-	Timer();
+	Timer(Vector2f position = Vector2f{ 0,0 }, string fontFile = "Fonts/IndieFlower.ttf" );
+
+	void setPosition(Vector2f position);
+
+	int getMinutes();
+
+	int getSeconds();
+
+	void draw(RenderWindow& window);
+
+	void resetTime();
 };
 
