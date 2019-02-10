f94b58f722067fde13c33550677d4e6dec35abaf
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-11 00:46:15 +0100

    Create MenuBar.hpp

---
+++ Logic_Game/Logic_Game/MenuBar.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include "Hearts.hpp"
+#include "LvInfo.hpp"
+#include "Timer.hpp"
+#include "Data.hpp"
+
+using namespace sf;
+using namespace std;
+
+class MenuBar{
+
+	Hearts hearts;
+	
+	LvInfo lvInfo;
+
+	Timer timer;
+
+public:
+
+	MenuBar();
+
+	Data getData();
+
+	void setHealth(int health);
+
+	void setLV(int lv);
+
+	void draw(RenderWindow& window);
+
+	void reset();
+};
+
