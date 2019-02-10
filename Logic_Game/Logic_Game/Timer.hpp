3670a969004d7d8a2f7b23c528704a180de82187
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-10 20:37:51 +0100

    Create Timer.hpp

---
+++ Logic_Game/Logic_Game/Timer.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+
+using namespace sf;
+
+class Timer : public Clock{
+
+
+public:
+
+	Timer();
+};
+
