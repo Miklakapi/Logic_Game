7295ff960cc18086f382a452bb6f2de5fca70f1a
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-10 20:37:31 +0100

    Create Map.hpp

---
+++ Logic_Game/Logic_Game/Map.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include <string>
+#include "Square.hpp"
+#include "MapReader.hpp"
+
+using namespace std;
+using namespace sf;
+
+class Map{
+
+	Square* square;
+
+	MapReader reader;
+
+public:
+
+	Map(string fileName = "Img/Square.png");
+
+	void setType(int number, Type type);
+
+	Type getType(int number);
+
+	void loadLV(int lv);
+
+	void renderLV();
+
+	void renderLV(int lv);
+
+	int getActualLV();
+
+	void draw(RenderWindow& window);
+
+	void reset();
+
+	~Map();
+};
+
