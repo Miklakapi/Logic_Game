505a85123b822d43f666a00e22527470ac791d24
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-11 20:59:20 +0100

    Create SlidingBlock.cpp

---
+++ Logic_Game/Logic_Game/SlidingBlock.cpp
@@ -0,0 +1,67 @@
+#include "SlidingBlock.hpp"
+
+Texture* SlidingBlock::texture;
+
+SlidingBlock::SlidingBlock(Vector2f position, string textureFile){
+	static int a = 0;
+	if (a == 0) {
+		texture = new Texture;
+		texture->loadFromFile(textureFile);
+		a++;
+	}
+	setSize(Vector2f{ 80,80 });
+	setTextureRect(IntRect{ 0,0,80,80 });
+	setPosition(position);
+	reset();
+}
+
+void SlidingBlock::destroy() {
+	clock.restart();
+	exist = false;
+	setTextureRect(IntRect{ 80,0,80,80 });
+}
+
+void SlidingBlock::push(Direction direction) {
+	if (moveNr != 10) return;
+	this->direction = direction;
+	if (direction == Direction::None) return;
+	moveNr = 0;
+}
+
+bool SlidingBlock::getExist() {
+	return exist;
+}
+
+void SlidingBlock::draw(RenderWindow& window) {
+	if (clock.getElapsedTime().asSeconds() >= 0.07 && moveNr < 10 && exist) {
+		clock.restart();
+		switch (direction) {
+		case Direction::Up:
+			move(Vector2f{ 0, -8 });
+			break;
+		case Direction::Down:
+			move(Vector2f{ 0, 8 });
+			break;
+		case Direction::Left:
+			move(Vector2f{ -8, 0 });
+			break;
+		case Direction::Right:
+			move(Vector2f{ 8, 0 });
+			break;
+		}
+		moveNr++;
+	}
+	else if (!exist) {
+		if (clock.getElapsedTime().asSeconds() >= 0.2) {
+			setTextureRect(IntRect{ 160,0,1,1 });
+		}
+	}
+	window.draw(*this);
+}
+
+void SlidingBlock::reset() {
+	exist = true;
+	moveNr = 10;
+	clock.restart();
+	direction = Direction::None;
+}
