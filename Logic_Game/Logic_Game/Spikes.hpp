72e26ccb310a873928ee31331b85dfe1e05f4167
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-11 16:28:37 +0100

    Create Spikes.hpp

---
+++ Logic_Game/Logic_Game/Spikes.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include <string>
+
+using namespace sf;
+using namespace std;
+
+class Spikes : RectangleShape{
+
+	Clock clock;
+
+	static Texture* texture;
+
+	static IntRect* rect;
+
+	bool dmg;
+
+	int stage;
+
+	enum Direction {
+		Up,
+		Down
+	};
+
+	Direction direction;
+
+public:
+
+	Spikes(Vector2f position = Vector2f{ 0,0 }, string textureFile = "Img/Spikes.png");
+	
+	bool getDmg();
+
+	void draw(RenderWindow& window);
+
+	void reset();
+};
+
