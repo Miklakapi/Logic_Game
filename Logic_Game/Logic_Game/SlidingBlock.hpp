d8e4bced99bfd34f91839727ba0bdcf7cc2c4a00
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-11 20:59:22 +0100

    Create SlidingBlock.hpp

---
+++ Logic_Game/Logic_Game/SlidingBlock.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include <string>
+
+using namespace sf;
+using namespace std;
+
+enum Direction {
+	Up,
+	Down,
+	Left,
+	Right,
+	None
+};
+
+class SlidingBlock : public RectangleShape{
+
+	static Texture* texture;
+
+	Direction direction;
+
+	Clock clock;
+
+	int moveNr;
+
+	bool exist;
+
+public:
+
+	SlidingBlock(Vector2f position = Vector2f{ 0,0 }, string textureFile = "Img/SlidingBlock.png");
+
+	void destroy();
+
+	void push(Direction direction);
+
+	bool getExist();
+
+	void draw(RenderWindow& window);
+
+	void reset();
+};
+
