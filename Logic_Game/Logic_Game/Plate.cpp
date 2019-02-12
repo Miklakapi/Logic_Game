eaff0f892d0513bd198579180ee821955d05cf05
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-12 21:56:36 +0100

    Update Plate.cpp

--- Logic_Game/Logic_Game/Plate.cpp
+++ Logic_Game/Logic_Game/Plate.cpp
@@ -1,12 +1,53 @@
 #include "Plate.hpp"
 
+Texture* Plate::texture;
 
+Plate::Plate(Vector2f position, Color color, string textureFile){
+	static int a = 0;
+	if (a == 0) {
+		texture = new Texture;
+		texture->loadFromFile(textureFile);
+		a++;
+	}
+	setSize(Vector2f{80,80});
+	setPosition(position);
+	setTexture(texture);
 
-Plate::Plate()
-{
+	background.setSize(Vector2f{ 80,80 });
+	background.setPosition(position);
+	background.setFillColor(color);
+
+	this->color = color;
+
+	reset();
 }
 
+bool Plate::isPressed() {
+	return pressed;
+}
 
-Plate::~Plate()
-{
+void Plate::setPressure(bool pressed) {
+	if (pressed && pressed != this->pressed) {
+		background.setFillColor(Color::Green);
+	}
+	else if ((!pressed) && pressed != this->pressed) {
+		background.setFillColor(color);
+	}
+	else {
+		return;
+	}
+	this->pressed = pressed;
+}
+
+void Plate::setColor(Color color) {
+	background.setFillColor(color);
+}
+
+void Plate::draw(RenderWindow& window) {
+	window.draw(background);
+	window.draw(*this);
+}
+
+void Plate::reset() {
+	setPressure(false);
 }
