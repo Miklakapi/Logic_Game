151a47285385f9db32569f898fad4fe3992119e0
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-12 21:56:38 +0100

    Update Plate.hpp

--- Logic_Game/Logic_Game/Plate.hpp
+++ Logic_Game/Logic_Game/Plate.hpp
@@ -1,8 +1,35 @@
 #pragma once
-class Plate
-{
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include <string>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+class Plate : public RectangleShape{
+
+	RectangleShape background;
+
+	bool pressed;
+
+	Color color;
+
+	static Texture* texture;
+
 public:
-	Plate();
-	~Plate();
+
+	Plate(Vector2f position = Vector2f{ 0,0 }, Color color = Color::Red, string textureFile = "Img/Plate.png");
+	
+	bool isPressed();
+
+	void setPressure(bool pressed);
+
+	void setColor(Color color);
+
+	void draw(RenderWindow& window);
+
+	void reset();
 };
 
