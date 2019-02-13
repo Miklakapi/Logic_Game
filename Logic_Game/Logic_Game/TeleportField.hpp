b8546afd1cd8d3d0b69cb22ad2ec7aacc1374f2c
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-13 12:19:49 +0100

    Update TeleportField.hpp

--- Logic_Game/Logic_Game/TeleportField.hpp
+++ Logic_Game/Logic_Game/TeleportField.hpp
@@ -2,15 +2,30 @@
 #include <SFML\Window.hpp>
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
+#include <string>
 
 using namespace sf;
+using namespace std;
 
-class TeleportField{
+class TeleportField : public RectangleShape{
+
+	RectangleShape background;
+
+	bool open;
+
+	Color color;
+
+	static Texture* texture;
 
 public:
 
-	TeleportField();
-	~TeleportField();
+	TeleportField(Vector2f position = Vector2f{ 0,0 }, Color color = Color::Red, string textureFile = "Img/Teleport.png");
+	
+	bool isOpen();
 
+	void setOpen(bool open);
+
+	void draw(RenderWindow& window);
+
+	void reset();
 };
-
