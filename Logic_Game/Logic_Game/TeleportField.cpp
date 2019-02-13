56155a2a57c4878324c04ac80c17ce487838dd82
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-13 12:19:48 +0100

    Update TeleportField.cpp

--- Logic_Game/Logic_Game/TeleportField.cpp
+++ Logic_Game/Logic_Game/TeleportField.cpp
@@ -1,10 +1,49 @@
 #include "TeleportField.hpp"
 
-TeleportField::TeleportField(){
+Texture* TeleportField::texture;
 
+TeleportField::TeleportField(Vector2f position, Color color, string textureFile){
+	static int a = 0;
+	if (a == 0) {
+		texture = new Texture;
+		texture->loadFromFile(textureFile);
+		a++;
+	}
+	setSize(Vector2f{ 80,80 });
+	setPosition(position);
+	setTexture(texture);
+
+	background.setSize(Vector2f{ 80,80 });
+	background.setPosition(position);
+	background.setFillColor(color);
+
+	this->color = color;
+
+	reset();
 }
 
+bool TeleportField::isOpen() {
+	return open;
+}
 
-TeleportField::~TeleportField(){
+void TeleportField::setOpen(bool open) {
+	if (open && open != this->open) {
+		background.setFillColor(Color::Green);
+	}
+	else if ((!open) && open != this->open) {
+		background.setFillColor(color);
+	}
+	else {
+		return;
+	}
+	this->open = open;
+}
 
+void TeleportField::draw(RenderWindow& window) {
+	window.draw(background);
+	window.draw(*this);
+}
+
+void TeleportField::reset() {
+	setOpen(false);
 }
