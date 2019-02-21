615ff1543d3eb50a9e406e31c5648fba3d4a8d69
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-21 02:33:46 +0100

    Update Main.cpp

--- Logic_Game/Logic_Game/Main.cpp
+++ Logic_Game/Logic_Game/Main.cpp
@@ -19,31 +19,34 @@ int main() {
 
 	Map map;
 	map.renderLV(1);
+	
 	MenuBar menuBar;
-	Spikes spikes[12];
-	Spikes* spike = spikes;
-	(spike + 0)->setPosition(VectorConverter::convert(6, 1).asVector2f());
-	(spike + 1)->setPosition(VectorConverter::convert(6, 2).asVector2f());
-	(spike + 2)->setPosition(VectorConverter::convert(6, 3).asVector2f());
-	(spike + 3)->setPosition(VectorConverter::convert(8, 1).asVector2f());
-	(spike + 4)->setPosition(VectorConverter::convert(8, 2).asVector2f());
-	(spike + 5)->setPosition(VectorConverter::convert(8, 3).asVector2f());
-	(spike + 6)->setPosition(VectorConverter::convert(9, 1).asVector2f());
-	(spike + 7)->setPosition(VectorConverter::convert(9, 2).asVector2f());
-	(spike + 8)->setPosition(VectorConverter::convert(9, 3).asVector2f());
-	(spike + 9)->setPosition(VectorConverter::convert(11, 1).asVector2f());
-	(spike + 10)->setPosition(VectorConverter::convert(11, 2).asVector2f());
-	(spike + 11)->setPosition(VectorConverter::convert(11, 3).asVector2f());
+
+	//Spikes spikes[12];
+	//Spikes* spike = spikes;
+	//(spike + 0)->setPosition(VectorConverter::convert(6, 1).asVector2f());
+	//(spike + 1)->setPosition(VectorConverter::convert(6, 2).asVector2f());
+	//(spike + 2)->setPosition(VectorConverter::convert(6, 3).asVector2f());
+	//(spike + 3)->setPosition(VectorConverter::convert(8, 1).asVector2f());
+	//(spike + 4)->setPosition(VectorConverter::convert(8, 2).asVector2f());
+	//(spike + 5)->setPosition(VectorConverter::convert(8, 3).asVector2f());
+	//(spike + 6)->setPosition(VectorConverter::convert(9, 1).asVector2f());
+	//(spike + 7)->setPosition(VectorConverter::convert(9, 2).asVector2f());
+	//(spike + 8)->setPosition(VectorConverter::convert(9, 3).asVector2f());
+	//(spike + 9)->setPosition(VectorConverter::convert(11, 1).asVector2f());
+	//(spike + 10)->setPosition(VectorConverter::convert(11, 2).asVector2f());
+	//(spike + 11)->setPosition(VectorConverter::convert(11, 3).asVector2f());
 
 	SlidingBlock block;
 	block.setPosition(VectorConverter::convert(2,2).asVector2f());
 
-	Plate plate(VectorConverter::convert(1, 1).asVector2f());
+	//Plate plate(VectorConverter::convert(1, 1).asVector2f());
 
-	Door door(VectorConverter::convert(3, 3).asVector2f());
+	//Door door(VectorConverter::convert(3, 3).asVector2f());
 
 	TeleportField teleport(VectorConverter::convert(4, 4).asVector2f());
 	teleport.setOpen(true);
+	teleport.setTeleportPlace(VectorConverter::convert(15, 5).asVector2f());
 
 	while (app.isOpen()) {
 
@@ -70,7 +73,7 @@ int main() {
 		}
 
 		if (block.getExist()) {
-			for (int i = 0; i < 12; i++) {
+			/*for (int i = 0; i < 12; i++) {
 				if (!(spike + i)->getDmg()) continue;
 				if (block.getPosition().x + 80 > (spike + i)->getPosition().x &&
 					block.getPosition().x < (spike + i)->getPosition().x + 80 &&
@@ -78,8 +81,8 @@ int main() {
 					block.getPosition().y < (spike + i)->getPosition().y + 80) {
 					block.destroy();
 				}
-			}
-			if (block.getPosition().x + 80 > plate.getPosition().x &&
+			}*/
+			/*if (block.getPosition().x + 80 > plate.getPosition().x &&
 				block.getPosition().x < plate.getPosition().x + 80 &&
 				block.getPosition().y + 80 >plate.getPosition().y &&
 				block.getPosition().y < plate.getPosition().y + 80) {
@@ -88,11 +91,10 @@ int main() {
 			else {
 				plate.setPressure(false);
 			}
-			door.setOpen(plate.isPressed());
+			door.setOpen(plate.isPressed());*/
 
-			if (block.getPosition().x == teleport.getPosition().x &&
-				block.getPosition().y == teleport.getPosition().y) {
-				block.setPosition(VectorConverter::convert(15, 5).asVector2f());
+			if (block.getPosition() == teleport.getPosition()) {
+				block.setPosition(teleport.getTeleportPlace());
 			}
 		}
 
@@ -102,11 +104,11 @@ int main() {
 		app.clear(Color::White);
 		map.draw(app);
 		menuBar.draw(app);
-		for (int i = 0; i < 12; i++) {
+		/*for (int i = 0; i < 12; i++) {
 			(spike + i)->draw(app);
 		}
 		plate.draw(app);
-		door.draw(app);
+		door.draw(app);*/
 		teleport.draw(app);
 		block.draw(app);
 		app.display();
