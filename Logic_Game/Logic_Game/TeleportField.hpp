8b54373829ed0142c25f0b34cfefb23d86a99d74
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-22 03:01:41 +0100

    Update TeleportField.hpp

--- Logic_Game/Logic_Game/TeleportField.hpp
+++ Logic_Game/Logic_Game/TeleportField.hpp
@@ -3,6 +3,9 @@
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
 #include <string>
+#include "Player.hpp"
+#include "SlidingBlock.hpp"
+#include "Mirror.hpp"
 
 using namespace sf;
 using namespace std;
@@ -13,25 +16,29 @@ class TeleportField : public RectangleShape{
 
 	bool open;
 
-	Color color;
-
 	static Texture* texture;
 
 	Vector2f teleportPlace;
 
 public:
 
-	TeleportField(Vector2f position = Vector2f{ 0,0 }, Color color = Color::Red, string textureFile = "Img/Teleport.png");
+	TeleportField(Vector2f position = Vector2f{ 0,0 });
 	
+	static void setTeleportTexture(string textureFile = "Img/Teleport.png");
+
+	void setTeleportPosition(Vector2f position);
+
+	void setTeleportSize(Vector2f size);
+
 	void setTeleportPlace(Vector2f position);
 
 	Vector2f getTeleportPlace();
 
-	bool isOpen();
-
 	void setOpen(bool open);
 
 	void draw(RenderWindow& window);
+
+	void run(Player& player, SlidingBlock* block, int number, Mirror* mirror, int number2);
 
 	void reset();
 };
