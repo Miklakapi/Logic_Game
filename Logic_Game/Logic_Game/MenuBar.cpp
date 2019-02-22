8e65781785daa5ad933d733c1fdfea3f984bb037
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-22 03:57:39 +0100

    Update MenuBar.cpp

--- Logic_Game/Logic_Game/MenuBar.cpp
+++ Logic_Game/Logic_Game/MenuBar.cpp
@@ -1,6 +1,31 @@
 #include "MenuBar.hpp"
 
-MenuBar::MenuBar() : hearts(Vector2f{20,15}), lvInfo(Vector2f{ 600,0 }), timer(Vector2f{ 1280,0 }) {
+MenuBar::MenuBar() : hearts(Vector2f{ 20,15 }), lvInfo(Vector2f{ 600,0 }), timer(Vector2f{ 1280,0 }) {
+	reset();
+}
+
+void MenuBar::setHeartsTexture(string textureFile) {
+	hearts.setHeartsTexture(textureFile);
+}
+
+void MenuBar::setInfoFont(string fontFile) {
+	lvInfo.setInfoFont(fontFile);
+}
+
+void MenuBar::setTimerFont(string fontFile) {
+	timer.setTimerFont(fontFile);
+}
+
+void MenuBar::setHeartsPosition(Vector2f position) {
+	hearts.setPosition(position);
+}
+
+void MenuBar::setInfoPosition(Vector2f position) {
+	lvInfo.setPosition(position);
+}
+
+void MenuBar::setTimerPosition(Vector2f position) {
+	timer.setPosition(position);
 }
 
 Data MenuBar::getData() {
