456832cfa7df71f4b980930b6735275b1d9ede0a
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-22 03:01:45 +0100

    Update Spikes.cpp

--- Logic_Game/Logic_Game/Spikes.cpp
+++ Logic_Game/Logic_Game/Spikes.cpp
@@ -4,28 +4,32 @@ Texture* Spikes::texture;
 
 IntRect* Spikes::rect;
 
-Spikes::Spikes(Vector2f position, string textureFile) {
-	static int a = 0;
-	if (a == 0) {
-		texture = new Texture;
-		texture->loadFromFile(textureFile);
-		rect = new IntRect[5];
-		*(rect + 0) = IntRect{0,0,80,80};
-		*(rect + 1) = IntRect{80,0,80,80};
-		*(rect + 2) = IntRect{160,0,80,80};
-		*(rect + 3) = IntRect{0,80,80,80};
-		*(rect + 4) = IntRect{80,80,80,80};
-		a++;
-	}
-	setSize(Vector2f{ 80,80 });
+Spikes::Spikes() {
 	setTexture(texture);
-	setPosition(position);
+	setSize(Vector2f{ 80,80 });
+	setPosition(Vector2f{ 0,0 });
 	reset();
 }
 
+void Spikes::loadSpikesTexture(string textureFile) {
+	static int a = 0;
+	if (a == 0) {
+		texture = new Texture;
+		rect = new IntRect[5];
+		*(rect + 0) = IntRect{ 0,0,80,80 };
+		*(rect + 1) = IntRect{ 80,0,80,80 };
+		*(rect + 2) = IntRect{ 160,0,80,80 };
+		*(rect + 3) = IntRect{ 0,80,80,80 };
+		*(rect + 4) = IntRect{ 80,80,80,80 };
+		a++;
+	}
+	texture->loadFromFile(textureFile);
+}
+
 void Spikes::setOn(bool on) {
+	if (!on && this->on != on) direction = Direction::Down;
+	else if (on && this->on != on) clock.restart();
 	this->on = on;
-	if (!on) direction = Direction::Down;
 }
 
 bool Spikes::isOn() {
@@ -43,7 +47,6 @@ void Spikes::run() {
 			clock.restart();
 			stage++;
 			setTextureRect(*(rect + stage));
-			dmg = true;
 		}
 		else if (stage == 4 && clock.getElapsedTime().asSeconds() >= 0.5) {
 			direction = Direction::Down;
@@ -62,7 +65,6 @@ void Spikes::run() {
 		}
 		else if (stage == 0) {
 			direction = Direction::Up;
-			dmg = false;
 		}
 	}
 }
@@ -71,9 +73,38 @@ void Spikes::draw(RenderWindow& window) {
 	window.draw(*this);
 }
 
+void Spikes::run(Player& player) {
+	if (stage == 0) return;
+	if (player.getPosition().x < getPosition().x + 80 && player.getPosition().x + 80 > getPosition().x &&
+		player.getPosition().y < getPosition().y + 80 && player.getPosition().y + 80 > getPosition().y) {
+		player.setLive(false);
+	}
+}
+
+void Spikes::run(SlidingBlock* block, int number) {
+	if (stage == 0) return;
+	for (int i = 0; i < number; i++) {
+		if (!(block + i)->getExist()) continue;
+		if ((block + i)->getPosition().x < getPosition().x + 80 && (block + i)->getPosition().x + 80 > getPosition().x &&
+			(block + i)->getPosition().y < getPosition().y + 80 && (block + i)->getPosition().y + 80 > getPosition().y) {
+			(block + i)->destroy();
+		}
+	}
+}
+
+void Spikes::run(Mirror* mirror, int number) {
+	if (stage == 0) return;
+	for (int i = 0; i < number; i++) {
+		if (!(mirror + i)->getExist()) continue;
+		if ((mirror + i)->getPosition().x < getPosition().x + 80 && (mirror + i)->getPosition().x + 80 > getPosition().x &&
+			(mirror + i)->getPosition().y < getPosition().y + 80 && (mirror + i)->getPosition().y + 80 > getPosition().y) {
+			(mirror + i)->destroy();
+		}
+	}
+}
+
 void Spikes::reset() {
 	setTextureRect(*rect);
-	dmg = false;
 	on = false;
 	stage = 0;
 	direction = Direction::Up;
