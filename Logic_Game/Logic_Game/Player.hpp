884067cbb5e9943963467eea54c82c3bd43e0ddd
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-22 15:09:36 +0100

    Update Player.hpp

--- Logic_Game/Logic_Game/Player.hpp
+++ Logic_Game/Logic_Game/Player.hpp
@@ -2,15 +2,69 @@
 #include <SFML\Window.hpp>
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
+#include <string>
+#include "Map.hpp"
+#include "VectorConverter.hpp"
+#include "Map.hpp"
+#include "Plate.hpp"
+#include "Door.hpp"
+#include "Spikes.hpp"
+#include "ShootingBlock.hpp"
+#include "SlidingBlock.hpp"
+#include "LaserMachine.hpp"
+#include "Mirror.hpp"
+
 
 using namespace sf;
+using namespace std;
 
 class Player : public RectangleShape{
 
 public:
+	
+	enum Direction {
+		Up,
+		Down,
+		Left,
+		Right,
+		None
+	};
 
-	Player();
+private:
+
+	Texture texture;
+
+	IntRect* rect;
+
+	int stage;
+
+	int moveNr;
+
+	Clock clockStage;
+
+	Clock clockMove;
+	
+	Player::Direction direction;
+
+	bool live;
+
+	void setStage(int stage);
+
+public:
+
+	Player(Vector2f position = Vector2f{ 0,0 }, string textureFile = "Img/Player.png");
+
+	void setPlayerTexture(string textureFile);
 
 	void setLive(bool live);
 
+	bool getLive();
+
+	void movePlayer(Direction direction);
+
+	void run();
+
+	void draw(RenderWindow& window);
+
+	void reset();
 };
