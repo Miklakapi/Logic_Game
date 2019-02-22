396cf2c9f9dffd292d19a9ba63970c4b6636de34
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-22 15:09:41 +0100

    Create LaserMachine.hpp

---
+++ Logic_Game/Logic_Game/LaserMachine.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include <string>
+
+
+using namespace sf;
+using namespace std;
+
+class LaserMachine : public RectangleShape{
+
+
+public:
+
+	LaserMachine();
+
+};
+
