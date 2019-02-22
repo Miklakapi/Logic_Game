697e8c5409a64d94f11b08067486e8f04cf66628
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-22 02:15:08 +0100

    Update Plate.cpp

--- Logic_Game/Logic_Game/Plate.cpp
+++ Logic_Game/Logic_Game/Plate.cpp
@@ -2,24 +2,31 @@
 
 Texture* Plate::texture;
 
-Plate::Plate(Vector2f position, Color color, string textureFile){
+Plate::Plate(){
+	setTexture(texture);
+	background.setFillColor(Color::Red);
+	setPlatePosition(Vector2f{ 0,0 });
+	setPlateSize(Vector2f{ 80,80 });
+	reset();
+}
+
+void Plate::setPlateTexture(string textureFile) {
 	static int a = 0;
 	if (a == 0) {
 		texture = new Texture;
-		texture->loadFromFile(textureFile);
 		a++;
 	}
-	setSize(Vector2f{80,80});
+	texture->loadFromFile(textureFile);
+}
+
+void Plate::setPlatePosition(Vector2f position) {
 	setPosition(position);
-	setTexture(texture);
-
-	background.setSize(Vector2f{ 80,80 });
 	background.setPosition(position);
-	background.setFillColor(color);
+}
 
-	this->color = color;
-
-	reset();
+void Plate::setPlateSize(Vector2f size) {
+	setSize(size);
+	background.setSize(size);
 }
 
 bool Plate::isPressed() {
@@ -31,7 +38,7 @@ void Plate::setPressure(bool pressed) {
 		background.setFillColor(Color::Green);
 	}
 	else if ((!pressed) && pressed != this->pressed) {
-		background.setFillColor(color);
+		background.setFillColor(Color::Red);
 	}
 	else {
 		return;
@@ -39,13 +46,43 @@ void Plate::setPressure(bool pressed) {
 	this->pressed = pressed;
 }
 
-void Plate::setColor(Color color) {
-	background.setFillColor(color);
-}
-
 void Plate::draw(RenderWindow& window) {
 	window.draw(background);
 	window.draw(*this);
+}
+
+bool Plate::run(Player& player) {
+	setPressure(false);
+	if (player.getPosition().x < getPosition().x + 80 && player.getPosition().x + 80 > getPosition().x &&
+		player.getPosition().y < getPosition().y + 80 && player.getPosition().y + 80 > getPosition().y) {
+		setPressure(true);
+		return true;
+	}
+	return false;
+}
+
+bool Plate::run(SlidingBlock* block, int number) {
+	setPressure(false);
+	for (int i = 0; i < number; i++) {
+		if ((block + i)->getPosition().x < getPosition().x + 80 && (block + i)->getPosition().x + 80 > getPosition().x &&
+			(block + i)->getPosition().y < getPosition().y + 80 && (block + i)->getPosition().y + 80 > getPosition().y) {
+			setPressure(true);
+			return true;
+		}
+	}
+	return false;
+}
+
+bool Plate::run(Mirror* mirror, int number) {
+	setPressure(false);
+	for (int i = 0; i < number; i++) {
+		if ((mirror + i)->getPosition().x < getPosition().x + 80 && (mirror + i)->getPosition().x + 80 > getPosition().x &&
+			(mirror + i)->getPosition().y < getPosition().y + 80 && (mirror + i)->getPosition().y + 80 > getPosition().y) {
+			setPressure(true);
+			return true;
+		}
+	}
+	return false;
 }
 
 void Plate::reset() {
