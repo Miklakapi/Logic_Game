3c072c69441c18dbec51110b980370f0838914cc
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-22 19:57:53 +0100

    Update Mirror.cpp

--- Logic_Game/Logic_Game/Mirror.cpp
+++ Logic_Game/Logic_Game/Mirror.cpp
@@ -1,7 +1,21 @@
 #include "Mirror.hpp"
 
+Texture* Mirror::texture;
+
 Mirror::Mirror(){
-	
+	setSize(Vector2f{ 80,80 });
+	setTexture(texture);
+	setPosition(Vector2f{ 0,0 });
+	reset();
+}
+
+void Mirror::setBlockTexture(string textureFile) {
+	static int a = 0;
+	if (a == 0) {
+		texture = new Texture;
+		a++;
+	}
+	texture->loadFromFile(textureFile);
 }
 
 void Mirror::destroy() {
@@ -10,4 +24,24 @@ void Mirror::destroy() {
 
 bool Mirror::getExist() {
 	return exist;
+}
+
+bool Mirror::push(Direction direction, Map& map, ShootingBlock* blockS, int number, Door* door, int number2,
+	LaserMachine* machine, int number3){// , SlidingBlock* block, int number4) {
+	return false;
+}
+
+void Mirror::run(Door* door, int number) {
+
+}
+
+void Mirror::draw(RenderWindow& window) {
+	window.draw(*this);
+}
+
+void Mirror::reset() {
+	exist = true;
+	moveNr = 10;
+	clock.restart();
+	direction = Direction::None;
 }
