cdf343dc8912d13dca1929aebf96efcf8d46ccd2
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-23 16:18:02 +0100

    Update ShootingBlock.cpp

--- Logic_Game/Logic_Game/ShootingBlock.cpp
+++ Logic_Game/Logic_Game/ShootingBlock.cpp
@@ -69,115 +69,119 @@ void ShootingBlock::setType(Type type) {
 		mine = new Mine[1];
 		switch (type) {
 		case A1:
-			mine->setMinePosition(Vector2f{ pos.x + 34,pos.y - 12 });
+			mine->setStartPosition(Vector2f{ pos.x + 34,pos.y - 12 });
 			mine->setDirection(Mine::Direction::Up);
 			break;
 		case A2:
-			mine->setMinePosition(Vector2f{ pos.x + 80,pos.y + 34 });
+			mine->setStartPosition(Vector2f{ pos.x + 80,pos.y + 34 });
 			mine->setDirection(Mine::Direction::Right);
 			break;
 		case A3:
-			mine->setMinePosition(Vector2f{ pos.x + 34,pos.y + 80 });
+			mine->setStartPosition(Vector2f{ pos.x + 34,pos.y + 80 });
 			mine->setDirection(Mine::Direction::Down);
 			break;
 		case A4:
-			mine->setMinePosition(Vector2f{ pos.x - 12,pos.y + 34 });
+			mine->setStartPosition(Vector2f{ pos.x - 12,pos.y + 34 });
 			mine->setDirection(Mine::Direction::Left);
 			break;
 		}
+		mineNr = 1;
 	}
 	else if (type > 3 && type < 10) {
 		mine = new Mine[2];
 		switch (type) {
 		case B1:
-			(mine + 0)->setMinePosition(Vector2f{ pos.x + 34,pos.y - 12 });
+			(mine + 0)->setStartPosition(Vector2f{ pos.x + 34,pos.y - 12 });
 			(mine + 0)->setDirection(Mine::Direction::Up);
-			(mine + 1)->setMinePosition(Vector2f{ pos.x + 80,pos.y + 34 });
+			(mine + 1)->setStartPosition(Vector2f{ pos.x + 80,pos.y + 34 });
 			(mine + 1)->setDirection(Mine::Direction::Right);
 			break;
 		case B2:
-			(mine + 0)->setMinePosition(Vector2f{ pos.x + 80,pos.y + 34 });
+			(mine + 0)->setStartPosition(Vector2f{ pos.x + 80,pos.y + 34 });
 			(mine + 0)->setDirection(Mine::Direction::Right);
-			(mine + 1)->setMinePosition(Vector2f{ pos.x + 34,pos.y + 80 });
+			(mine + 1)->setStartPosition(Vector2f{ pos.x + 34,pos.y + 80 });
 			(mine + 1)->setDirection(Mine::Direction::Down);
 			break;
 		case B3:
-			(mine + 0)->setMinePosition(Vector2f{ pos.x + 34,pos.y + 80 });
+			(mine + 0)->setStartPosition(Vector2f{ pos.x + 34,pos.y + 80 });
 			(mine + 0)->setDirection(Mine::Direction::Down);
-			(mine + 1)->setMinePosition(Vector2f{ pos.x - 12,pos.y + 34 });
+			(mine + 1)->setStartPosition(Vector2f{ pos.x - 12,pos.y + 34 });
 			(mine + 1)->setDirection(Mine::Direction::Left);
 			break;
 		case B4:
-			(mine + 0)->setMinePosition(Vector2f{ pos.x - 12,pos.y + 34 });
+			(mine + 0)->setStartPosition(Vector2f{ pos.x - 12,pos.y + 34 });
 			(mine + 0)->setDirection(Mine::Direction::Left);
-			(mine + 1)->setMinePosition(Vector2f{ pos.x + 34,pos.y - 12 });
+			(mine + 1)->setStartPosition(Vector2f{ pos.x + 34,pos.y - 12 });
 			(mine + 1)->setDirection(Mine::Direction::Up);
 			break;
 		case B5:
-			(mine + 0)->setMinePosition(Vector2f{ pos.x + 34,pos.y - 12 });
+			(mine + 0)->setStartPosition(Vector2f{ pos.x + 34,pos.y - 12 });
 			(mine + 0)->setDirection(Mine::Direction::Up);
-			(mine + 1)->setMinePosition(Vector2f{ pos.x + 34,pos.y + 80 });
+			(mine + 1)->setStartPosition(Vector2f{ pos.x + 34,pos.y + 80 });
 			(mine + 1)->setDirection(Mine::Direction::Down);
 			break;
 		case B6:
-			(mine + 0)->setMinePosition(Vector2f{ pos.x - 12,pos.y + 34 });
+			(mine + 0)->setStartPosition(Vector2f{ pos.x - 12,pos.y + 34 });
 			(mine + 0)->setDirection(Mine::Direction::Left);
-			(mine + 1)->setMinePosition(Vector2f{ pos.x + 80,pos.y + 34 });
+			(mine + 1)->setStartPosition(Vector2f{ pos.x + 80,pos.y + 34 });
 			(mine + 1)->setDirection(Mine::Direction::Right);
 			break;
 		}
+		mineNr = 2;
 	}
 	else if (type > 9 && type < 14) {
 		mine = new Mine[3];
 		switch (type) {
 		case C1:
-			(mine + 0)->setMinePosition(Vector2f{ pos.x - 12,pos.y + 34 });
+			(mine + 0)->setStartPosition(Vector2f{ pos.x - 12,pos.y + 34 });
 			(mine + 0)->setDirection(Mine::Direction::Left);
-			(mine + 1)->setMinePosition(Vector2f{ pos.x + 34,pos.y - 12 });
+			(mine + 1)->setStartPosition(Vector2f{ pos.x + 34,pos.y - 12 });
 			(mine + 1)->setDirection(Mine::Direction::Up);
-			(mine + 2)->setMinePosition(Vector2f{ pos.x + 80,pos.y + 34 });
+			(mine + 2)->setStartPosition(Vector2f{ pos.x + 80,pos.y + 34 });
 			(mine + 2)->setDirection(Mine::Direction::Right);
 			break;
 		case C2:
-			(mine + 0)->setMinePosition(Vector2f{ pos.x + 34,pos.y - 12 });
+			(mine + 0)->setStartPosition(Vector2f{ pos.x + 34,pos.y - 12 });
 			(mine + 0)->setDirection(Mine::Direction::Up);
-			(mine + 1)->setMinePosition(Vector2f{ pos.x + 80,pos.y + 34 });
+			(mine + 1)->setStartPosition(Vector2f{ pos.x + 80,pos.y + 34 });
 			(mine + 1)->setDirection(Mine::Direction::Right);
-			(mine + 2)->setMinePosition(Vector2f{ pos.x + 34,pos.y + 80 });
+			(mine + 2)->setStartPosition(Vector2f{ pos.x + 34,pos.y + 80 });
 			(mine + 2)->setDirection(Mine::Direction::Down);
 			break;
 		case C3:
-			(mine + 0)->setMinePosition(Vector2f{ pos.x + 80,pos.y + 34 });
+			(mine + 0)->setStartPosition(Vector2f{ pos.x + 80,pos.y + 34 });
 			(mine + 0)->setDirection(Mine::Direction::Right);
-			(mine + 1)->setMinePosition(Vector2f{ pos.x + 34,pos.y + 80 });
+			(mine + 1)->setStartPosition(Vector2f{ pos.x + 34,pos.y + 80 });
 			(mine + 1)->setDirection(Mine::Direction::Down);
-			(mine + 2)->setMinePosition(Vector2f{ pos.x - 12,pos.y + 34 });
+			(mine + 2)->setStartPosition(Vector2f{ pos.x - 12,pos.y + 34 });
 			(mine + 2)->setDirection(Mine::Direction::Left);
 			break;
 		case C4:
-			(mine + 0)->setMinePosition(Vector2f{ pos.x + 34,pos.y + 80 });
+			(mine + 0)->setStartPosition(Vector2f{ pos.x + 34,pos.y + 80 });
 			(mine + 0)->setDirection(Mine::Direction::Down);
-			(mine + 1)->setMinePosition(Vector2f{ pos.x - 12,pos.y + 34 });
+			(mine + 1)->setStartPosition(Vector2f{ pos.x - 12,pos.y + 34 });
 			(mine + 1)->setDirection(Mine::Direction::Left);
-			(mine + 2)->setMinePosition(Vector2f{ pos.x + 34,pos.y - 12 });
+			(mine + 2)->setStartPosition(Vector2f{ pos.x + 34,pos.y - 12 });
 			(mine + 2)->setDirection(Mine::Direction::Up);
 			break;
 		}
+		mineNr = 3;
 	}
 	else if (type == 14) {
 		mine = new Mine[4];
 		switch (type) {
 		case D1:
-			(mine + 0)->setMinePosition(Vector2f{ pos.x + 34,pos.y - 12 });
+			(mine + 0)->setStartPosition(Vector2f{ pos.x + 34,pos.y - 12 });
 			(mine + 0)->setDirection(Mine::Direction::Up);
-			(mine + 1)->setMinePosition(Vector2f{ pos.x + 80,pos.y + 34 });
+			(mine + 1)->setStartPosition(Vector2f{ pos.x + 80,pos.y + 34 });
 			(mine + 1)->setDirection(Mine::Direction::Right);
-			(mine + 2)->setMinePosition(Vector2f{ pos.x + 34,pos.y + 80 });
+			(mine + 2)->setStartPosition(Vector2f{ pos.x + 34,pos.y + 80 });
 			(mine + 2)->setDirection(Mine::Direction::Down);
-			(mine + 3)->setMinePosition(Vector2f{ pos.x - 12,pos.y + 34 });
+			(mine + 3)->setStartPosition(Vector2f{ pos.x - 12,pos.y + 34 });
 			(mine + 3)->setDirection(Mine::Direction::Left);
 			break;
 		}
+		mineNr = 4;
 	}
 }
 
@@ -201,26 +205,28 @@ bool ShootingBlock::isOn() {
 void ShootingBlock::draw(RenderWindow& window) {
 	window.draw(background);
 	window.draw(*this);
-	if (type < 4) window.draw(*mine);
+	if (type < 4) (mine + 0)->draw(on, window);
 	else if (type >= 4 && type < 10) {
-		window.draw(*(mine + 0));
-		window.draw(*(mine + 1));
+		(mine + 0)->draw(on, window);
+		(mine + 1)->draw(on, window);
 	}
 	else if (type >= 10 && type < 14) {
-		window.draw(*(mine + 0));
-		window.draw(*(mine + 1));
-		window.draw(*(mine + 2));
+		(mine + 0)->draw(on, window);
+		(mine + 1)->draw(on, window);
+		(mine + 2)->draw(on, window);
 	}
 	else if (type == 14) {
-		window.draw(*(mine + 0));
-		window.draw(*(mine + 1));
-		window.draw(*(mine + 2));
-		window.draw(*(mine + 3));
+		(mine + 0)->draw(on, window);
+		(mine + 1)->draw(on, window);
+		(mine + 2)->draw(on, window);
+		(mine + 3)-> draw(on, window);
 	}
 }
 
-void ShootingBlock::run(Map& map, Door* door, int number) {
-
+void ShootingBlock::run(Map& map, Door* door, int number, ShootingBlock* block, int number2) {
+	for (int i = 0; i < mineNr; i++) {
+		(mine + i)->run(on, map, door, number);
+	}
 }
 
 void ShootingBlock::reset() {
