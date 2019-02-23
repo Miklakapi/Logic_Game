090f0624e37e2ca722a941faf40a955f13a7b110
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-23 04:25:59 +0100

    Update Mine.cpp

--- Logic_Game/Logic_Game/Mine.cpp
+++ Logic_Game/Logic_Game/Mine.cpp
@@ -1,13 +1,31 @@
 #include "Mine.hpp"
 
-Mine::Mine(){
+Texture* Mine::texture;
 
+Mine::Mine(){
+	setSize(Vector2f{ 12,12 });
+	setTexture(texture);
+}
+
+void Mine::setMineTexture(string textureFile) {
+	static int a = 0;
+	if (a == 0) {
+		texture = new Texture;
+		a++;
+	}
+	texture->loadFromFile(textureFile);
 }
 
 void Mine::setMinePosition(Vector2f position) {
 	startPosition = position;
+	reset();
 }
 
 void Mine::setDirection(Direction direction) {
 	this->direction = direction;
+}
+
+void Mine::reset() {
+	exist = false;
+	setPosition(startPosition);
 }
