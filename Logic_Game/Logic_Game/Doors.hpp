1f7ef14e6ea8fa91a2f7fb821ca387d5f135d78a
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-02-25 17:32:24 +0100

    Create Doors.hpp

---
+++ Logic_Game/Logic_Game/Doors.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include <string>
+#include "Door.hpp"
+#include "Laser.hpp"
+#include "Player.hpp"
+#include "ShootingBlock.hpp"
+#include "SlidingBlock.hpp"
+#include "LaserMachine.hpp"
+#include "Mirror.hpp"
+
+using namespace sf;
+using namespace std;
+
+class Doors{
+
+	Door* door;
+
+	int number;
+
+public:
+	
+	Doors(int number, string textureFile = "Img/Door.png");
+	
+	void setDoorPosition(int number, Vector2f position);
+
+	void setDoorSize(int number, Vector2f size);
+
+	bool isOpen(int number);
+
+	void setOpen(int number, bool open, Player& player, SlidingBlock* block, int number2, Mirror* mirror, int number3);
+
+	Door* getDoor();
+
+	int getNumber();
+
+	void draw(RenderWindow& window);
+
+	void reset(int number);
+
+	void reset();
+
+	~Doors();
+};
+
