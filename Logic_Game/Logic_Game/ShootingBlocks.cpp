e648b977f0a2ad9a4b4a17219e57f31241466cf4
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-03-03 17:34:56 +0100

    Update ShootingBlocks.cpp

--- Logic_Game/Logic_Game/ShootingBlocks.cpp
+++ Logic_Game/Logic_Game/ShootingBlocks.cpp
@@ -15,7 +15,7 @@ void ShootingBlocks::setSize(int number, Vector2f size) {
 	(block + number)->setSize(size);
 }
 
-void ShootingBlocks::setDelay(int number, int delay) {
+void ShootingBlocks::setDelay(int number, float delay) {
 	(block + number)->setDelay(delay);
 }
 
@@ -150,123 +150,6 @@ void ShootingBlocks::reset() {
 	}
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ShootingBlocks::~ShootingBlocks(){
 	delete [] block;
 }
-
-
-
-
-/*
-int Mine::run(bool on, Map& map, Door* door, int number) {
-	if (on == false && exist == false) return -1;
-	if (on == true && exist == false) {
-		VectorConverter vec(getPosition());
-		if (map.getType(vec.asNumber()) == Square::Wall) return -1;
-		exist = true;
-	}
-	if (clock.getElapsedTime().asSeconds() < 0.03 || exist == false) return -1;
-	clock.restart();
-	VectorConverter vec(getPosition());
-	for (int i = 0; i < number; i++) {
-		if ((door + i)->isOpen()) continue;
-		if ((door + i)->getPosition() == VectorConverter::convert(vec.asXY().x, vec.asXY().y).asVector2f()) {
-			reset();
-			return -1;
-		}
-	}
-	if (moveNr == 8) {
-		switch (direction) {
-		case Up:
-			if (map.getType(vec.asNumber() - 18) == Square::Wall) {
-				reset();
-				return -1;
-			}
-			for (int i = 0; i < number; i++) {
-				if ((door + i)->isOpen()) continue;
-				if ((door + i)->getPosition() == VectorConverter::convert(vec.asXY().x, vec.asXY().y - 1).asVector2f()) {
-					reset();
-					return -1;
-				}
-			}
-			break;
-		case Down:
-			if (map.getType(vec.asNumber() + 18) == Square::Wall) {
-				reset();
-				return -1;
-			}
-			for (int i = 0; i < number; i++) {
-				if ((door + i)->isOpen()) continue;
-				if ((door + i)->getPosition() == VectorConverter::convert(vec.asXY().x, vec.asXY().y + 1).asVector2f()) {
-					reset();
-					return -1;
-				}
-			}
-			break;
-		case Left:
-			if (map.getType(vec.asNumber() - 1) == Square::Wall) {
-				reset();
-				return -1;
-			}
-			for (int i = 0; i < number; i++) {
-				if ((door + i)->isOpen()) continue;
-				if ((door + i)->getPosition() == VectorConverter::convert(vec.asXY().x - 1, vec.asXY().y).asVector2f()) {
-					reset();
-					return -1;
-				}
-			}
-			break;
-		case Right:
-			if (map.getType(vec.asNumber() + 1) == Square::Wall) {
-				reset();
-				return -1;
-			}
-			for (int i = 0; i < number; i++) {
-				if ((door + i)->isOpen()) continue;
-				if ((door + i)->getPosition() == VectorConverter::convert(vec.asXY().x + 1, vec.asXY().y).asVector2f()) {
-					reset();
-					return -1;
-				}
-			}
-			break;
-		}
-	}
-	switch (direction) {
-	case Direction::Up:
-		move(Vector2f{ 0, -8 });
-		break;
-	case Direction::Down:
-		move(Vector2f{ 0, 8 });
-		break;
-	case Direction::Left:
-		move(Vector2f{ -8, 0 });
-		break;
-	case Direction::Right:
-		move(Vector2f{ 8, 0 });
-		break;
-	}
-	moveNr++;
-	if (moveNr == 10) moveNr = 0;
-	return moveNr;
-}
-*/
