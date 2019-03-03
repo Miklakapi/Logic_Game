1f416d43ffc0cc260d287e0a4aac938a5040ecb8
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-03-03 17:35:02 +0100

    Update LaserMachine.hpp

--- Logic_Game/Logic_Game/LaserMachine.hpp
+++ Logic_Game/Logic_Game/LaserMachine.hpp
@@ -3,17 +3,73 @@
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
 #include <string>
-
+#include "Laser.hpp"
 
 using namespace sf;
 using namespace std;
 
 class LaserMachine : public RectangleShape{
 
+public:
+
+	enum Type {
+		A1,
+		A2,
+		A3,
+		A4,
+		B1,
+		B2,
+		B3,
+		B4,
+		B5,
+		B6,
+		C1,
+		C2,
+		C3,
+		C4,
+		D1,
+	};
+
+private:
+
+	Laser* laser;
+
+	int laserNr;
+
+	static Texture* texture;
+
+	static IntRect* rect;
+
+	Type type;
+
+	RectangleShape background;
+
+	bool on;
 
 public:
 
 	LaserMachine();
 
+	static void setMachineTexture(string textureFile = "Img/LaserMachine.png");
+
+	void setMachinePosition(Vector2f position);
+
+	void setMachineSize(Vector2f size);
+
+	Laser* getLaser();
+
+	int getLaserNr();
+
+	void setType(Type type);
+
+	Type getType();
+
+	void setOn(bool on);
+
+	bool isOn();
+
+	void draw(RenderWindow& window);
+
+	void reset();
 };
 
