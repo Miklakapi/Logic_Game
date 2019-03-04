f3fbd9f71760d3bc8a73ceab4d9e95b0182bc04c
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-03-04 18:22:26 +0100

    Update Mirrors.hpp

--- Logic_Game/Logic_Game/Mirrors.hpp
+++ Logic_Game/Logic_Game/Mirrors.hpp
@@ -1,8 +1,19 @@
 #pragma once
-class Mirrors
-{
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include "Mirror.hpp"
+
+class Mirrors{
+
+	Mirror* mirror;
+
+	int number;
+
 public:
+	
 	Mirrors();
+
 	~Mirrors();
 };
 
