7886c290ca8cdb0ce4f3cb792c03232393ae90ac
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-03-04 16:36:57 +0100

    Update LaserMachines.cpp

--- Logic_Game/Logic_Game/LaserMachines.cpp
+++ Logic_Game/Logic_Game/LaserMachines.cpp
@@ -158,15 +158,63 @@ void LaserMachines::setType(int number, LaserMachine::Type type, Map& map,
 		}
 	}
 	else if (type < 10 && type >3) {
-
+		switch (type) {
+		case 4:
+			(laser + 0)->setPosition(vec.asVector2f(), Laser::Up, up);
+			(laser + 1)->setPosition(vec.asVector2f(), Laser::Right, right);
+			break;
+		case 5:
+			(laser + 0)->setPosition(vec.asVector2f(), Laser::Right, right);
+			(laser + 1)->setPosition(vec.asVector2f(), Laser::Down, down);
+			break;
+		case 6:
+			(laser + 0)->setPosition(vec.asVector2f(), Laser::Down, down);
+			(laser + 1)->setPosition(vec.asVector2f(), Laser::Left, left);
+			break;
+		case 7:
+			(laser + 0)->setPosition(vec.asVector2f(), Laser::Left, left);
+			(laser + 1)->setPosition(vec.asVector2f(), Laser::Up, up);
+			break;
+		case 8:
+			(laser + 0)->setPosition(vec.asVector2f(), Laser::Up, up);
+			(laser + 1)->setPosition(vec.asVector2f(), Laser::Down, down);
+			break;
+		case 9:
+			(laser + 0)->setPosition(vec.asVector2f(), Laser::Left, left);
+			(laser + 1)->setPosition(vec.asVector2f(), Laser::Right, right);
+			break;
+		}
 	}
 	else if (type < 14 && type > 9) {
-
+		switch (type) {
+		case 10:
+			(laser + 0)->setPosition(vec.asVector2f(), Laser::Left, left);
+			(laser + 1)->setPosition(vec.asVector2f(), Laser::Up, up);
+			(laser + 2)->setPosition(vec.asVector2f(), Laser::Right, right);
+			break;
+		case 11:
+			(laser + 0)->setPosition(vec.asVector2f(), Laser::Up, up);
+			(laser + 1)->setPosition(vec.asVector2f(), Laser::Right, right);
+			(laser + 2)->setPosition(vec.asVector2f(), Laser::Down, down);
+			break;
+		case 12:
+			(laser + 0)->setPosition(vec.asVector2f(), Laser::Right, right);
+			(laser + 1)->setPosition(vec.asVector2f(), Laser::Down, down);
+			(laser + 2)->setPosition(vec.asVector2f(), Laser::Left, left);
+			break;
+		case 13:
+			(laser + 0)->setPosition(vec.asVector2f(), Laser::Down, down);
+			(laser + 1)->setPosition(vec.asVector2f(), Laser::Left, left);
+			(laser + 2)->setPosition(vec.asVector2f(), Laser::Up, up);
+			break;
+		}
 	}
 	else if (type == 14) {
-
+		(laser + 0)->setPosition(vec.asVector2f(), Laser::Down, down);
+		(laser + 1)->setPosition(vec.asVector2f(), Laser::Left, left);
+		(laser + 2)->setPosition(vec.asVector2f(), Laser::Up, up);
+		(laser + 3)->setPosition(vec.asVector2f(), Laser::Right, right);
 	}
-	
 }
 
 LaserMachine::Type LaserMachines::getType(int number) {
@@ -195,9 +243,114 @@ void LaserMachines::draw(RenderWindow& window) {
 	}
 }
 
-void LaserMachines::run(Map& map, SlidingBlock* block, int number, Mirror* mirror, int number2, Door* door, int number3,
-	ShootingBlock* blockS, int number4) {
+void LaserMachines::run(SlidingBlock* block, int number, Mirror* mirror, int number2, Door* door, int number3) {
 
+	//Machine
+	for (int i = 0; i < this->number; i++) {
+		if (!(machine + i)->isOn()) return;
+
+		Laser* laser = (machine + i)->getLaser();
+		LaserMachine::Type type = (machine + i)->getType();
+
+		int nr = 0;
+
+		if (type < 4) nr = 1;
+		else if (type < 10 && type > 3) nr = 2;
+		else if (type < 14 && type > 9) nr = 3;
+		else if (type == 14) nr = 4;
+
+		//Laser
+		for (int j = 0; j < nr; j++) {
+
+			(laser + j)->on();
+			SimpleLaser* simLaser = (laser + j)->getSimpleLaser();
+			bool wall(false);
+
+			//Simple Laser
+			for (int k = 0; k < (laser + j)->getLaserNr(); k++) {
+				
+				if (wall) {
+					(simLaser + k)->setExist(false);
+					continue;
+				}
+
+				Vector2f vec = (simLaser + k)->getPosition();
+				SimpleLaser::Direction direction = (simLaser + k)->getDirection();
+				if (direction == SimpleLaser::Direction::Horizontal) {
+					vec = Vector2f{ vec.x,vec.y + 35 };
+				}
+				else {
+					vec = Vector2f{ vec.x + 35,vec.y };
+				}
+
+
+				for (int l = 0; l < number; l++) {
+					if (!(block + l)->getExist()) continue;
+					Vector2f vec2 = (block + l)->getPosition();
+					if (direction == SimpleLaser::Direction::Horizontal) {
+						if (vec.x + 80 > vec2.x && vec.x < vec2.x + 80 &&
+							vec.y + 10 > vec2.y && vec.y < vec2.y + 80) {
+							wall = true;
+							(simLaser + k)->setExist(false);
+							break;;
+						}
+					}
+					else {
+						if (vec.x + 10 > vec2.x && vec.x < vec2.x + 80 &&
+							vec.y + 80 > vec2.y && vec.y < vec2.y + 80) {
+							wall = true;
+							(simLaser + k)->setExist(false);
+							break;
+						}
+					}
+				}
+				if (wall) continue;
+
+				for (int l = 0; l < number2; l++) {
+					if (!(mirror + l)->getExist()) continue;
+					Vector2f vec2 = (mirror + l)->getPosition();
+					if (direction == SimpleLaser::Direction::Horizontal) {
+						if (vec.x + 80 > vec2.x && vec.x < vec2.x + 80 &&
+							vec.y + 10 > vec2.y && vec.y < vec2.y + 80) {
+							wall = true;
+							(simLaser + k)->setExist(false);
+							break;;
+						}
+					}
+					else {
+						if (vec.x + 10 > vec2.x && vec.x < vec2.x + 80 &&
+							vec.y + 80 > vec2.y && vec.y < vec2.y + 80) {
+							wall = true;
+							(simLaser + k)->setExist(false);
+							break;
+						}
+					}
+				}
+				if (wall) continue;
+
+				for (int l = 0; l < number3; l++) {
+					if ((door + l)->isOpen()) continue;
+					Vector2f vec2 = (door + l)->getPosition();
+					if (direction == SimpleLaser::Direction::Horizontal) {
+						if (vec.x + 80 > vec2.x && vec.x < vec2.x + 80 &&
+							vec.y + 10 > vec2.y && vec.y < vec2.y + 80) {
+							wall = true;
+							(simLaser + k)->setExist(false);
+							break;;
+						}
+					}
+					else {
+						if (vec.x + 10 > vec2.x && vec.x < vec2.x + 80 &&
+							vec.y + 80 > vec2.y && vec.y < vec2.y + 80) {
+							wall = true;
+							(simLaser + k)->setExist(false);
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
 void LaserMachines::reset(int number) {
