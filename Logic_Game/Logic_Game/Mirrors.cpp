3f0e1011bd58e2a50eeab05b8e1470c755e9127d
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-03-04 23:03:24 +0100

    Update Mirrors.cpp

--- Logic_Game/Logic_Game/Mirrors.cpp
+++ Logic_Game/Logic_Game/Mirrors.cpp
@@ -1,7 +1,49 @@
 #include "Mirrors.hpp"
 
-Mirrors::Mirrors(){
+Mirrors::Mirrors(int number, string textureFile){
+	Mirror::setBlockTexture(textureFile);
+	mirror = new Mirror[number];
+	this->number = number;
+}
 
+
+
+
+
+
+
+
+
+
+
+/*bool push(int number, SlidingBlock::Direction direction, Map& map,
+	Door* door, int number3, ShootingBlock* blockS, int number4,
+	LaserMachine* machine, int number5);*/
+
+Mirror* Mirrors::getMirror() {
+	return mirror;
+}
+
+int Mirrors::getNumber() {
+	return number;
+}
+
+void Mirrors::draw(RenderWindow& window) {
+	for (int i = 0; i < number; i++) {
+		(mirror + i)->draw(window);
+	}
+}
+
+//void Mirrors::run();
+
+void Mirrors::reset(int number) {
+	(mirror + number)->reset();
+}
+
+void Mirrors::reset() {
+	for (int i = 0; i < number; i++) {
+		(mirror + i)->reset();
+	}
 }
 
 Mirrors::~Mirrors(){
