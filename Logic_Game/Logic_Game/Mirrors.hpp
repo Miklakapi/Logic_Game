373b3e8cc776802284c92db39be27654c0d4ea25
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-03-04 23:03:22 +0100

    Update Mirrors.hpp

--- Logic_Game/Logic_Game/Mirrors.hpp
+++ Logic_Game/Logic_Game/Mirrors.hpp
@@ -3,6 +3,14 @@
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
 #include "Mirror.hpp"
+#include "Map.hpp"
+#include "SlidingBlock.hpp"
+#include "Door.hpp"
+#include "ShootingBlock.hpp"
+#include "LaserMachine.hpp"
+
+using namespace sf;
+using namespace std;
 
 class Mirrors{
 
@@ -12,8 +20,46 @@ class Mirrors{
 
 public:
 	
-	Mirrors();
+	Mirrors(int number, string textureFile = "Img/Mirror.png");
+
+	void setPosition(int number, Vector2f position);
+
+	void setSize(int number, Vector2f size);
+
+	Laser* getLaser(int number);
+
+	int getLaserNr(int number);
+
+	void setType(int number, Mirror::Type type, Map& map, 
+		ShootingBlock* blocks, int number2, LaserMachine machine, int number3);
+
+	Mirror::Type getType(int number);
+
+	void setOn(int number, bool on);
+
+	bool isOn(int number);
+
+	int getMoveNumber(int number);
+
+	void destroy(int number);
+
+	bool getExist(int number);
+
+	bool push(int number, SlidingBlock::Direction direction, Map& map, 
+		Door* door, int number3, ShootingBlock* blockS, int number4,
+		LaserMachine* machine, int number5);
+
+	Mirror* getMirror();
+
+	int getNumber();
+
+	void draw(RenderWindow& window);
+
+	void run();
+
+	void reset(int number);
+
+	void reset();
 
 	~Mirrors();
 };
-
