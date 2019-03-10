4f55094709344e2d5ec593dc710edf0600bf2b6c
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-03-10 13:22:37 +0100

    Update HelpClass.hpp

--- Logic_Game/Logic_Game/HelpClass.hpp
+++ Logic_Game/Logic_Game/HelpClass.hpp
@@ -10,6 +10,10 @@
 #include "Doors.hpp"
 #include "ShootingBlocks.hpp"
 #include "LaserMachines.hpp"
+#include "Traps.hpp"
+#include "TeleportFields.hpp"
+#include "Plates.hpp"
+#include "MenuBar.hpp"
 
 using namespace sf;
 
@@ -20,9 +24,9 @@ public:
 	static void move(Player& player, Map& map, SlidingBlock* block, int number, Mirror* mirror, int number2, Door* door, int number3,
 		ShootingBlock* blockS, int number4, LaserMachine* machine, int number5);
 	
-	static void runAll(Map& map, Player& player, SlidingBlocks& block, Mirrors& mirror, Doors& door, ShootingBlocks& blockS, LaserMachines& machine);
+	static void runAll(MenuBar& menu, Map& map, TeleportFields& fields, Plates& plates, Traps& traps, Player& player, SlidingBlocks& block, Mirrors& mirror, Doors& door, ShootingBlocks& blockS, LaserMachines& machine);
 
 	static void resetAll(Map& map, Player& player, SlidingBlocks& block, Mirrors& mirror, Doors& door, ShootingBlocks& blockS, LaserMachines& machine);
 
-	static void drawAll(RenderWindow& window, Map& map, Player& player, SlidingBlocks& block, Mirrors& mirror, Doors& door, ShootingBlocks& blockS, LaserMachines& machine);
+	static void drawAll(RenderWindow& window, Map& map, TeleportFields& fields, Plates& plates, Traps& traps, Player& player, SlidingBlocks& block, Mirrors& mirror, Doors& door, ShootingBlocks& blockS, LaserMachines& machine);
 };
