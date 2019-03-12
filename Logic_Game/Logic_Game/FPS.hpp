bf283e0c5861907377caef4b9a9d13c4d66f74d3
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-03-12 17:45:57 +0100

    Update FPS.hpp

--- Logic_Game/Logic_Game/FPS.hpp
+++ Logic_Game/Logic_Game/FPS.hpp
@@ -15,17 +15,11 @@ class FPS : public Text{
 
 	Clock clock;
 
-	bool on;
-
 public:
 
 	FPS(Vector2f position = Vector2f{ 3,825 }, string fontFile = "Fonts/IndieFlower.ttf");
 
 	void setFPSFont(string fontFile);
-
-	void setOn(bool on);
-
-	bool isOn();
 
 	void run();
 };
