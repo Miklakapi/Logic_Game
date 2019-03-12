d0623af505f08d9ff8ad109b966d8bfc5aa4cd3e
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-03-12 19:04:36 +0100

    Update Play.hpp

--- Logic_Game/Logic_Game/Play.hpp
+++ Logic_Game/Logic_Game/Play.hpp
@@ -2,11 +2,28 @@
 #include <SFML\Window.hpp>
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
+#include "VectorConverter.hpp"
+#include "Map.hpp"
+#include "MenuBar.hpp"
+#include "SlidingBLocks.hpp"
+#include "Mirrors.hpp"
+#include "LaserMachines.hpp"
+#include "ShootingBlocks.hpp"
+#include "Plates.hpp"
+#include "Doors.hpp"
+#include "TeleportFields.hpp"
+#include "Traps.hpp"
+#include "Player.hpp"
+#include "HelpClass.hpp"
+
+
+
 
 using namespace sf;
 using namespace std;
 
 class Play{
+
 
 
 
