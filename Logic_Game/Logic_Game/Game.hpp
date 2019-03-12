43a1b0e2770f10e8294a8d85f317a995266b13f2
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-03-12 17:45:35 +0100

    Update Game.hpp

--- Logic_Game/Logic_Game/Game.hpp
+++ Logic_Game/Logic_Game/Game.hpp
@@ -2,10 +2,17 @@
 #include <SFML\Window.hpp>
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
+#include <string>
+#include <fstream>
 #include "Intro.hpp"
 #include "Menu.hpp"
+#include "Play.hpp"
+
+#include "Options.hpp"
+#include "FPS.hpp"
 
 using namespace sf;
+using namespace std;
 
 class Game{
 
@@ -23,13 +30,20 @@ class Game{
 	Menu* menu;
 
 
+	Options* options;
 
+	FPS* FpsCounter;
 
 	Type type;
 
+	bool sound;
+	bool fpsCounter;
+
+	Clock delayClick;
+
 public:
 
-	Game();
+	Game(RenderWindow& window);
 
 	void run(RenderWindow& window);
 
