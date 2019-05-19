0c40942faacffdb1c0b9e7c2db3d871b874d9f94
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-05-19 15:47:34 +0200

    Update LaserMachines.hpp

--- Logic_Game/Logic_Game/LaserMachines.hpp
+++ Logic_Game/Logic_Game/LaserMachines.hpp
@@ -1,8 +1,11 @@
 #pragma once
+
 #include <SFML\Window.hpp>
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
+
 #include <string>
+
 #include "LaserMachine.hpp"
 #include "VectorConverter.hpp"
 #include "Map.hpp"
