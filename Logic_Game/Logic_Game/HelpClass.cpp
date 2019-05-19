33ad253063cbf31dc02c610dace0d79a22ad994d
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-05-19 19:12:21 +0200

    Update HelpClass.cpp

--- Logic_Game/Logic_Game/HelpClass.cpp
+++ Logic_Game/Logic_Game/HelpClass.cpp
@@ -1,7 +1,7 @@
 #include "HelpClass.hpp"
 
 void HelpClass::move(Player& player, Map& map, SlidingBlock* block, int number, Mirror* mirror, int number2, Door* door, int number3,
-	ShootingBlock* blockS, int number4, LaserMachine* machine, int number5) {
+	ShootingBlock* blockS, int number4, LaserMachine* machine, int number5, LaserReceiver* receiver, int number6) {
 
 	bool end(false);
 
@@ -13,7 +13,7 @@ void HelpClass::move(Player& player, Map& map, SlidingBlock* block, int number, 
 	else if (Keyboard::isKeyPressed(Keyboard::D)) direction = Player::Right;
 	else end = true;
 
-	if (player.movePlayer(direction, map, block, number, mirror, number2, door, number3, blockS, number4, machine, number5) && player.getLive() && end == false) {
+	if (player.movePlayer(direction, map, block, number, mirror, number2, door, number3, blockS, number4, machine, number5, receiver, number6) && player.getLive() && end == false) {
 		VectorConverter vec{ player.getPosition() };
 		SlidingBlock::Direction direction1 = SlidingBlock::None;
 		Mirror::Direction direction2 = Mirror::None;
@@ -93,33 +93,41 @@ void HelpClass::move(Player& player, Map& map, SlidingBlock* block, int number, 
 			}
 		}
 
+		for (int i = 0; i < number6 && end == false; i++) {
+			if ((receiver + i)->getPosition() == vec2.asVector2f()) {
+				end = true;
+				break;
+			}
+		}
+
 		if (end == false) {
 			if (nr != -1) {
 				(block + nr)->push(direction1);
-				player.movePlayer(direction, map, block, number, mirror, number2, door, number3, blockS, number4, machine, number5);
+				player.movePlayer(direction, map, block, number, mirror, number2, door, number3, blockS, number4, machine, number5, receiver, number6);
 			}
 
 			if (nr2 != -1) {
 				(mirror + nr2)->push(direction2);
-				player.movePlayer(direction, map, block, number, mirror, number2, door, number3, blockS, number4, machine, number5);
+				player.movePlayer(direction, map, block, number, mirror, number2, door, number3, blockS, number4, machine, number5, receiver, number6);
 			}
 		}
 	}
 	player.run(blockS, number4);
 }
 
-void HelpClass::runAll(MenuBar& menu, Map& map, TeleportFields& fields, Plates& plates, Traps& traps, Player& player, SlidingBlocks& block, Mirrors& mirror, Doors& door, ShootingBlocks& blockS, LaserMachines& machine) {
+void HelpClass::runAll(MenuBar& menu, Map& map, TeleportFields& fields, Plates& plates, Traps& traps, Player& player, SlidingBlocks& block, Mirrors& mirror, Doors& door, ShootingBlocks& blockS, LaserMachines& machine, LaserReceivers& receivers) {
 	menu.run();
 	plates.run(player, block.getBlock(), block.getNumber(), mirror.getMirror(), mirror.getNumber());
 	fields.run(player, block.getBlock(), block.getNumber(), mirror.getMirror(), mirror.getNumber());
 	block.run();
-	mirror.run(map,block.getBlock(),block.getNumber(),door.getDoor(),door.getNumber(),blockS.getBlock(),blockS.getNumber(),machine.getMachine(), machine.getNumber());
+	mirror.run(map, block.getBlock(), block.getNumber(), door.getDoor(), door.getNumber(), blockS.getBlock(), blockS.getNumber(), machine.getMachine(), machine.getNumber(), receivers.getReceiver(), receivers.getNumber());
 	traps.run(player, block.getBlock(), block.getNumber(), mirror.getMirror(), mirror.getNumber());
 	machine.run(block.getBlock(), block.getNumber(), mirror.getMirror(), mirror.getNumber(), door.getDoor(), door.getNumber());
-	blockS.run(map, door.getDoor(), door.getNumber(), block.getBlock(), block.getNumber(), mirror.getMirror(), mirror.getNumber(), machine.getMachine(), machine.getNumber());
+	blockS.run(map, door.getDoor(), door.getNumber(), block.getBlock(), block.getNumber(), mirror.getMirror(), mirror.getNumber(), machine.getMachine(), machine.getNumber(), receivers.getReceiver(), receivers.getNumber());
+	receivers.run(mirror.getMirror(), mirror.getNumber(), machine.getMachine(), machine.getNumber());
 }
 
-void HelpClass::resetAll(MenuBar& menu, Map& map, Player& player, SlidingBlocks& block, Mirrors& mirror, Doors& door, ShootingBlocks& blockS, LaserMachines& machine) {
+void HelpClass::resetAll(MenuBar& menu, Map& map, Player& player, SlidingBlocks& block, Mirrors& mirror, Doors& door, ShootingBlocks& blockS, LaserMachines& machine, LaserReceivers& receivers) {
 	map.reset();
 	player.reset();
 	block.reset();
@@ -128,9 +136,10 @@ void HelpClass::resetAll(MenuBar& menu, Map& map, Player& player, SlidingBlocks&
 	blockS.reset();
 	machine.reset();
 	menu.reset();
+	receivers.reset();
 }
 
-void HelpClass::drawAll(RenderWindow& window, MenuBar& menu, Map& map, TeleportFields& fields, Plates& plates,Traps& traps, Player& player, SlidingBlocks& block, Mirrors& mirror, Doors& door, ShootingBlocks& blockS, LaserMachines& machine) {
+void HelpClass::drawAll(RenderWindow& window, MenuBar& menu, Map& map, TeleportFields& fields, Plates& plates,Traps& traps, Player& player, SlidingBlocks& block, Mirrors& mirror, Doors& door, ShootingBlocks& blockS, LaserMachines& machine, LaserReceivers& receivers) {
 	map.draw(window);
 	fields.draw(window);
 	plates.draw(window);
@@ -141,5 +150,6 @@ void HelpClass::drawAll(RenderWindow& window, MenuBar& menu, Map& map, TeleportF
 	door.draw(window);
 	blockS.draw(window);
 	machine.draw(window);
+	receivers.draw(window);
 	menu.draw(window);
 }
