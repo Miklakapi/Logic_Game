272b5085d8a6f34298e1d92fc39b053f168e609a
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-05-19 15:26:39 +0200

    Update Play.hpp

--- Logic_Game/Logic_Game/Play.hpp
+++ Logic_Game/Logic_Game/Play.hpp
@@ -1,7 +1,9 @@
 #pragma once
+
 #include <SFML\Window.hpp>
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
+
 #include "VectorConverter.hpp"
 #include "Map.hpp"
 #include "MenuBar.hpp"
@@ -15,9 +17,6 @@
 #include "LaserMachines.hpp"
 #include "Mirrors.hpp"
 #include "HelpClass.hpp"
-
-
-
 
 using namespace sf;
 using namespace std;
