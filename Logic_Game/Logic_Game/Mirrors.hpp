cdae53a1c42c26249734bfb74646b39d244a6b98
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-05-19 19:12:00 +0200

    Update Mirrors.hpp

--- Logic_Game/Logic_Game/Mirrors.hpp
+++ Logic_Game/Logic_Game/Mirrors.hpp
@@ -1,13 +1,16 @@
 #pragma once
+
 #include <SFML\Window.hpp>
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
+
 #include "Mirror.hpp"
 #include "Map.hpp"
 #include "SlidingBlock.hpp"
 #include "Door.hpp"
 #include "ShootingBlock.hpp"
 #include "LaserMachine.hpp"
+#include "LaserReceiver.hpp"
 
 using namespace sf;
 using namespace std;
@@ -30,7 +33,7 @@ public:
 
 	int getLaserNr(int number);
 
-	void setType(int number, Mirror::Type type, Map& map, ShootingBlock* blocks, int number2, LaserMachine* machine, int number3);
+	void setType(int number, Mirror::Type type, Map& map, ShootingBlock* blocks, int number2, LaserMachine* machine, int number3, LaserReceiver* receiver, int number4);
 
 	Mirror::Type getType(int number);
 
@@ -52,7 +55,7 @@ public:
 	void draw(RenderWindow& window);
 
 	void run(Map& map, SlidingBlock* block, int number, Door* door, int number2, ShootingBlock* blockS, int number3,
-		LaserMachine* machine, int number4);
+		LaserMachine* machine, int number4, LaserReceiver* receiver, int number5);
 
 	void reset(int number);
 
