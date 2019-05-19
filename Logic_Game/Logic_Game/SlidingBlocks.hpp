a9b25e3e5fc65e6856b613a05a218c47003fedc0
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2019-05-19 19:12:12 +0200

    Update SlidingBlocks.hpp

--- Logic_Game/Logic_Game/SlidingBlocks.hpp
+++ Logic_Game/Logic_Game/SlidingBlocks.hpp
@@ -1,8 +1,11 @@
 #pragma once
+
 #include <SFML\Window.hpp>
 #include <SFML\System.hpp>
 #include <SFML\Graphics.hpp>
+
 #include <string>
+
 #include "VectorConverter.hpp"
 #include "SlidingBlock.hpp"
 #include "Map.hpp"
@@ -10,6 +13,7 @@
 #include "Door.hpp"
 #include "ShootingBlock.hpp"
 #include "LaserMachine.hpp"
+#include "LaserReceiver.hpp"
 
 using namespace sf;
 using namespace std;
@@ -33,7 +37,7 @@ public:
 	bool getExist(int number);
 
 	bool push(int number, SlidingBlock::Direction direction, Map& map, Mirror* mirror, int number2, Door* door, int number3,
-		ShootingBlock* blockS, int number4, LaserMachine* machine, int number5);
+		ShootingBlock* blockS, int number4, LaserMachine* machine, int number5, LaserReceiver* receiver, int number6);
 
 	SlidingBlock* getBlock();
 
